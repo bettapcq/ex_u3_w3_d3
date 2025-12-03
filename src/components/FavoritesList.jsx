52d78493ff7c52d74ae965832ebf61f131f4d0a3
Elisabetta Piacquadio <betta.pcq@gmail.com>
2025-12-03 16:11:10 +0100

    fixed favorites list page

--- src/components/FavoritesList.jsx
+++ src/components/FavoritesList.jsx
@@ -2,23 +2,12 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { MdOutlineFavorite } from 'react-icons/md';
-import { MdOutlineFavoriteBorder } from 'react-icons/md';
 
-const FavoritesList = ({ data }) => {
+const FavoritesList = () => {
   const favoritesArray = useSelector((currentState) => {
     return currentState.favorites.content;
   });
   const dispatch = useDispatch();
-
-  const isFavorite = favoritesArray.includes(data.company_name);
-
-  const changeIcon = () => {
-    isFavorite
-      ? dispatch({ type: 'REMOVE_FROM_FAVORITES', payload: data.company_name })
-      : dispatch({ type: 'ADD_TO_FAVORITES', payload: data.company_name });
-  };
-
-  console.log(favoritesArray);
 
   return (
     <Container>
@@ -27,14 +16,16 @@ const FavoritesList = ({ data }) => {
           <h1 className="display-4">Your favorites companies: </h1>
           {favoritesArray.map((company) => (
             <>
-              <div type="button" className="me-2" onClick={() => changeIcon()}>
-                {isFavorite ? (
-                  <MdOutlineFavorite />
-                ) : (
-                  <MdOutlineFavoriteBorder />
-                )}
+              <div
+                type="button"
+                className="me-2"
+                onClick={() =>
+                  dispatch({ type: 'REMOVE_FROM_FAVORITES', payload: company })
+                }
+              >
+                <MdOutlineFavorite />
               </div>
-              <Link>{company.company_name}</Link>
+              <Link>{company}</Link>
             </>
           ))}
         </Col>
