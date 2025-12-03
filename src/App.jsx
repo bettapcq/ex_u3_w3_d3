98ad26186b20b5cf38726f35d33ce1936354b50a
Elisabetta Piacquadio <betta.pcq@gmail.com>
2025-12-03 16:24:08 +0100

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,14 @@ import CompanySearchResults from './components/CompanySearchResults';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { GrFavorite } from 'react-icons/gr';
 import FavoritesList from './components/FavoritesList';
+import MyNavBar from './components/MyNavBar';
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <Link to="/favorites" className="p-5 fs-1">
-          <GrFavorite />
-        </Link>
+      <MyNavBar/>
         <Routes>
           <Route path="/" element={<MainSearch />} />
           <Route path="/:company" element={<CompanySearchResults />} />
